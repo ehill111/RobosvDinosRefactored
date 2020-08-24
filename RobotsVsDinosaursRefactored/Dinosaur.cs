a53f530990a7c69fc5265a8fc2a41dbcd65c55f8
Eric M Hill <ehill111@comcast.net>
2020-08-24 08:53:58 -0400

    Began refactoring of entire Robot vs Dinosaurs game. Implementing things I've learned since submitting assignment. 1. Created classes. 2. Created variables for robots and dinosaurs. 3. Created parent class (Opponents) for Robot and Dinosaurs.

---
+++ RobotsVsDinosaursRefactored/Dinosaur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaursRefactored
+{
+    class Dinosaur : Opponents
+    {//Member variables have/has a/an...
+        string type;
+        int energy;
+
+
+    }
+}
---
+++ RobotsVsDinosaursRefactored/Robot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaursRefactored
+{
+    class Robot : Opponents
+    {
+        //Member variables have/has a/an...
+        string name;
+        int powerLevel;
+        Weapon type;
+
+    }
+}
